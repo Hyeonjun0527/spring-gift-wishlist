0dcdd478ae0fdbe86355f2886e752d206a467809
HyeonJun <osumaniaddict527@gmail.com>
2025-07-10 15:35:44 +0900

    refactor(all):리뷰 반영 - 테스트 환경 bean 분리   - TestConfig, MockJwtTokenProvider   MockJwtTokenProvider는 MemberInteractor에서 사용중이기에 Mock가 필요했음.   - admin.enabled, jwt.enabled 설정 값 조건부 로딩 추가   - 현재 상황에 불필요한 @Order 제거 - ProductTest, ProductControllerTest에서 @ActiveProfiles 제거 - JwtTokenProvider에서 isValid 메서드를 isNotValid로 변경 - JwtAuthenticationFilter 가독성 향상 - WebConfig와 AuthorizationAspect 등에서 혼란을 야기하는 profile 기반 설정 +  ConditionalOnProperty에서 오직 ConditionalOnProperty로만 관리

--- src/test/java/gift/common/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/gift/common/exception/GlobalExceptionHandlerTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,7 +19,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest(ProductController.class)
-@ActiveProfiles("test")
 class GlobalExceptionHandlerTest {
 
     @Autowired
